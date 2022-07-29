fb25d66105d482694f55e60782d478e9566b3d27
Guillem Borrell <Borrell.Guillem@bcg.com>
2022-07-29 10:54:33 +0200

    First running command

---
+++ helloworld/__init__.py
@@ -0,0 +1,4 @@
+"""
+Added a docstring because flit told me to do so
+"""
+__version__ = "0.0.1"
---
+++ helloworld/run-hello.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from azure.ai.ml import MLClient, command
+from azureml.core import Workspace, Experiment
+from azure.identity import DefaultAzureCredential
+
+import helloworld
+
+base_path = Path(helloworld.__file__).parent
+config = base_path / "config.json"
+print(config.resolve())
+
+ws = Workspace.from_config(config)
+default_azure_credential = DefaultAzureCredential()
+
+ml_client = MLClient(
+    default_azure_credential, ws.subscription_id, ws.resource_group, ws.name
+)
+
+experiment = Experiment(ws, "test_experiment")
+
+# Assumes a previously created compute cluster
+compute_name = "compute-cluster"
+
+job = command(
+    code=base_path / "src",
+    command="python hello.py",
+    environment="AzureML-sklearn-1.0-ubuntu20.04-py38-cpu@latest",
+    compute=compute_name,
+    display_name="hello-world-example-1",
+    experiment_name=experiment.name,
+)
+
+returned_job = ml_client.create_or_update(job)
+print(returned_job.studio_url)
